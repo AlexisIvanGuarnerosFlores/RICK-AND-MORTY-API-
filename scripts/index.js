7d16827dbaffb7baaeff75760ea3ab5ae611a3a4
Alexis Ivan Guarneros Flores <alexisalekey@gmail.com>
2022-08-31 23:03:20 -0500

    Mejorando API, cards y navbar

--- scripts/index.js
+++ scripts/index.js
@@ -47,19 +47,61 @@ const print = (data) => {
         divPrincipal.removeChild(divPrincipal.firstChild);
     }
 
-    data.forEach(element => {
-        let grid_item = document.createElement('div');
-        grid_item.classList.add('grid-item');
+    // OPCION 1
 
-        grid_item.style.backgroundImage = `url(${element.image})`;
-        // grid_item.style.backgroundAttachment = 'fixed';
-        grid_item.style.backgroundSize = 'contain';
-        // grid_item.style.backgroundPosition = 'center center';
+    // data.forEach(element => {
+    //     let grid_item = document.createElement('div');
+    //     grid_item.classList.add('grid-item');
+
+    //     grid_item.style.backgroundImage = `url(${element.image})`;
+        
+    //     grid_item.style.backgroundSize = 'contain';
+    //     // grid_item.style.backgroundAttachment = 'fixed';
+    //     // grid_item.style.backgroundPosition = 'center center';
 
 
-        divPrincipal.appendChild(grid_item);
-    });
+    //     divPrincipal.appendChild(grid_item);
+    // });
+
+
+
+
+
+
+// OPCION 2
+
+for (let n in data) {
+    
+    document.querySelector("#gallery-container").innerHTML +=
+    `
+    
+    <div class="card bg-success" >
+    <img src="${data[n].image}" class="card-img-top" alt="...">
+    
+  <div class="card-body bg-warning">
+    <h5 class="card-title ">${data[n].name}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    
+    <li class="list-group-item bg-transparent">Species: ${data[n].species}</li>
+    <li class="list-group-item bg-info">Gender: ${data[n].gender}</li>
+    <li class="list-group-item bg-info">Origin: ${data[n].origin.name}</li>
+  </ul>
+  <!-- <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div> -->
+</div>`
+   }
+
+
+
+
+
 }
+
+                 
+        
 
 
 
